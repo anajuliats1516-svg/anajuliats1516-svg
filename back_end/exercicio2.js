3e5c0a1d001f91892a3fa149542736b6e6a60cdf
anajuliats1516-svg <anajuliats1516@gmail.com>
2026-02-25 09:51:11 -0300

    Add HTML program to calculate absolute value

---
+++ back_end/exercicio2.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Programa que retorna o valor abasoluto de um número</title>
+</head>
+Autor: ana julia
+
+<body>
+  <script>
+    var valor1 = Math.abs (-1234);
+    alert (valor1);
+  </script>
+</body>
+</html>
