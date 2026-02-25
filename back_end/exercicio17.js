253f205194d98e013848abd91e3fcfbc5b9d6c80
anajuliats1516-svg <anajuliats1516@gmail.com>
2026-02-25 10:59:11 -0300

    Create exercicio17.js for tangent calculation
    Add HTML file to calculate tangent of a number using JavaScript.

---
+++ back_end/exercicio17.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Retorna a tangente de um número, que é equivalente a divisão do seno pelo cosseno deste mesmo valor</title>
+</head>
+Autor: Ana Julia
+
+<body>
+  <script>
+    var valor1 = Math.tan (1.5);
+    alert (valor1);
+  </script>
+</body>
+</html>
+
