3a9d82e25c59216f8e4eb204a4ee49618323fed5
anajuliats1516-svg <anajuliats1516@gmail.com>
2026-02-25 10:50:26 -0300

    Create exercicio11.js

---
+++ back_end/exercicio11.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Este método retorna o menor valor entre dois números</title>
+</head>
+Autor: Ana Julia
+
+<body>
+  <script>
+    var valor1 = Math.min ( 3,9);
+    alert (valor1);
+  </script>
+</body>
+</html>
+
