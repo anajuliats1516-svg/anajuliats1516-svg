2c1fa755746a3c9f020274cb944717884f2352c5
anajuliats1516-svg <anajuliats1516@gmail.com>
2026-02-25 10:14:16 -0300

    Create exercicio5.js

---
+++ back_end/exercicio5.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Desse modo será arredondado para 15.</title>
+</head>
+Autor: Ana Julia
+
+<body>
+  <script>
+    var valor1 = Math.ceil (-14.6);
+    alert (valor1);
+  </script>
+</body>
+</html>
