964b8e67d58b7271a757fa980df961afb24444be
anajuliats1516-svg <anajuliats1516@gmail.com>
2026-02-25 10:51:56 -0300

    Create exercicio12.js

---
+++ back_end/exercicio12.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna a base elevada à potência do expoente.</title>
+</head>
+Autor: Ana Julia
+
+<body>
+  <script>
+    var valor1 = Math.pow ( 1024,2);
+    alert (valor1);
+  </script>
+</body>
+</html>
